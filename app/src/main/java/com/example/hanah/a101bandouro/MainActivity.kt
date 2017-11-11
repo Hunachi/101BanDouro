f659f9e3ce314c11e96de6c81bec43397ae04097
DESKTOP-34ARI2H\hanah <hanahanahunachi@gmail.com>
2017-11-12 01:11:07 +0900

    再生は不可能。

---
+++ app/src/main/java/com/example/hanah/a101bandouro/MainActivity.kt
@@ -0,0 +1,112 @@
+package com.example.hanah.a101bandouro
+
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.location.LocationProvider
+import android.support.v4.app.ActivityCompat
+import android.widget.TextView
+import android.content.Intent
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import android.Manifest
+import android.content.Context
+
+import com.nifty.cloud.mb.core.NCMB
+
+class MainActivity : AppCompatActivity(), LocationListener, MainFragment.Callback {
+
+    private var locationManager: LocationManager? = null
+    private var fragment: MainFragment? = null
+    private val nowStationName = "po"
+    private var textView1: TextView? = null
+    private var textView2: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView1 = findViewById(R.id.text_view1)
+        textView2 = findViewById(R.id.text_view2)
+
+        //first setting
+        NCMB.initialize(
+                this,
+                "4bf00452b48657802fb11f962c23564281cf5abd3ef0bb2483104378dda55441",
+                "4b020547b0b0803b9136c1a84c5c62d48659a142fa16c9a3f43a567e50b0e270"
+        )
+
+        //fragment
+        fragment = MainFragment(this, this)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1000)
+        } else {
+            locationStart()
+        }
+
+    }
+
+    private fun locationStart() {
+        Log.d("debug", "locationStart()")
+        // LocationManager インスタンス生成
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val gpsEnabled = locationManager!!.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        if (!gpsEnabled) {
+            // GPSを設定するように促す
+            val settingsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(settingsIntent)
+            Log.d("debug", "not gpsEnable, startActivity")
+        } else {
+            Log.d("debug", "gpsEnabled")
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1000)
+            Log.d("debug", "checkSelfPermission false")
+            return
+        }
+        locationManager!!.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 80f, this)
+    }
+
+    // 結果の受け取り
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == 1000) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("debug", "checkSelfPermission true")
+                locationStart()
+                return
+            } else {
+                // それでも拒否された時の対応
+                val toast = Toast.makeText(this, "これ以上なにもできません", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
+    }
+
+    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+        when (status) {
+            LocationProvider.AVAILABLE -> Log.d("debug", "LocationProvider.AVAILABLE")
+            LocationProvider.OUT_OF_SERVICE -> Log.d("debug", "LocationProvider.OUT_OF_SERVICE")
+            LocationProvider.TEMPORARILY_UNAVAILABLE -> Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE")
+        }
+    }
+
+    override fun onLocationChanged(location: Location) {
+        // 緯度の表示
+        textView1!!.text = "Latitude(緯度):" + location.latitude
+        // 経度の表示
+        textView2!!.text = "Longitude(経度):" + location.longitude
+        fragment!!.getNearStation(/*location.longitude*/35.6783055555, /*location.latitude*/139.77044166)
+    }
+
+    override fun onProviderEnabled(provider: String) {}
+    override fun onProviderDisabled(provider: String) {}
+
+    override fun setText(station: String) {
+        textView1!!.text = station + "付近"
+    }
+}
--- app/src/main/java/com/example/hanah/a101bandouro/MainFragment.kt
+++ app/src/main/java/com/example/hanah/a101bandouro/MainFragment.kt
@@ -1,60 +1,120 @@
 package com.example.hanah.a101bandouro
 
+import android.annotation.SuppressLint
+import android.media.AudioFormat
+import android.media.AudioManager
+import android.media.AudioTrack
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import com.example.hanah.a101bandouro.client.ServerClient
+import com.nifty.cloud.mb.core.FetchFileCallback
+import com.nifty.cloud.mb.core.NCMBException
+import com.nifty.cloud.mb.core.NCMBFile
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import java.sql.ResultSet
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import android.os.Environment.getExternalStorageDirectory
+import java.io.IOException
+import android.R.raw
+import android.content.Context
+import io.reactivex.Single
 
+
+@SuppressLint("ValidFragment")
 /**
  * Created by hanah on 2017/11/11.
  */
-class MainFragment: android.support.v4.app.Fragment() {
-    var activity_: MainActivity? = null
-    companion object {
+class MainFragment(val callback: Callback, context: Context) : android.support.v4.app.Fragment() {
 
-    }
-    fun newInstance(pointX: Double, pointY: Double, nowStationName: String): MainFragment{
-        val fragment = MainFragment()
-        val args = Bundle()
-        args.putDouble("x", pointX)
-        args.putDouble("y", pointY)
-        args.putString("station", nowStationName)
-        Log.d(pointX.toString()+" : "+ pointY.toString(), nowStationName)
-        fragment.arguments = args
-        return fragment
+    private lateinit var mediaPlayer: MediaPlayer
+    private var station = ""
+    private var audioTrack: AudioTrack? = null
+    private var contexts = context
+
+    interface Callback {
+        fun setText(station: String)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mediaPlayer = MediaPlayer()
+        val file = NCMBFile(station + "2" + ".wav")
+        file.fetchInBackground(FetchFileCallback() { bytes: ByteArray, ncmbException: NCMBException? ->
+            audioTrack = AudioTrack(
+                    AudioManager.STREAM_MUSIC,
+                    22050,
+                    AudioFormat.CHANNEL_OUT_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    bytes.size,
+                    AudioTrack.MODE_STREAM
+            )
+        })
         Log.d("hoge", "hoge")
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        activity_ = activity as MainActivity?
     }
 
-    fun getNearStation(){
+    fun getNearStation(pointX: Double, pointY: Double) {
         var newStation: String
-        val client = ServerClient("KEY")
+        val client = ServerClient("eBBWPyXMYduCN759")
         client
-                .findStation(arguments.getDouble("x",
-                        33.589085), arguments.getDouble("y",130.398821))
+                .findStation(pointX, pointY)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
                     newStation = it.ResultSet.Point.Station.Name
-                    Log.d("近くの駅",newStation)
-                },{
+                    callback.setText(newStation)
+                    Log.d("近くの駅", newStation)
+                    if (station != newStation) {
+                        station = newStation
+                        getData(station = newStation)
+                    }
+                }, {
                     it.printStackTrace()
                 })
     }
 
+    fun getData(station: String) {
+        val file = NCMBFile(station + "2" + ".wav")
+        file.fetchInBackground(FetchFileCallback() { bytes: ByteArray?, ncmbException: NCMBException? ->
 
+            /*if (bytes != null) {
+                audioTrack = AudioTrack(
+                        AudioManager.STREAM_MUSIC,
+                        22050,
+                        AudioFormat.CHANNEL_OUT_MONO,
+                        AudioFormat.ENCODING_PCM_16BIT,
+                        bytes.size,
+                        AudioTrack.MODE_STREAM
+                )
+                audioTrack?.write(bytes, 0, bytes.size)
+                if (audioTrack != null) audioTrack?.play()
+            }
 
-    fun getData(){
+*/
+            /*val tempMp3 = File.createTempFile(station, ".mp3", contexts.cacheDir)
+            tempMp3.deleteOnExit()
+            FileOutputStream(tempMp3).run {
+                write(bytes)
+                close()
+            }
+            mediaPlayer.reset()
+            mediaPlayer.setDataSource(
+                    FileInputStream(tempMp3).fd
+            )
+            mediaPlayer.prepare()
+            mediaPlayer.start()*/
+
+        })
+    }
+
+    fun changeTasteful(tasteful: Int) {
 
     }
 }
